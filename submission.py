f184683a9f2ca1b17225b85b859c79450accd2b1
anushreep2018 <anushreep2018@email.iimcal.ac.in>
2017-04-08 23:45:12 +0530

    Update submission.py

--- submission.py
+++ submission.py
@@ -6,7 +6,7 @@ Created on Sat Apr  8 02:14:06 2017
 @author: Anushree
 
 
-Created of Python 3.5
+Created on Python 3.5
 
 
 We are trying to find out smallest prime factor of  number of questions in each contest.
